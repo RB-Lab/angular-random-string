72564cdc785d67fdc39ff312a15dbfece3ecbfc8
Roman Bekkiev <rbekkiev@Romans-Mac-mini.local>
2014-12-22 14:06:42 +0300

    add code

---
+++ src/angular-random-string.js
@@ -0,0 +1,18 @@
+// originally insprired by https://github.com/klughammer/node-randomstring
+
+angular.module('randomString', []).factory('randomString', ['$window', function randomStringFactory(w){
+
+	var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+	var Math = w.Math;
+
+	return function randomString(length) {
+		length = length || 10;
+		var string = '', rnd;
+		while (length > 0) {
+			rnd = Math.floor(Math.random() * chars.length);
+			string += chars.charAt(rnd);
+			length--;
+		}
+		return string;
+	};
+}]);
